d988e1342e784bc821ad5112521f91f2b8a07f90
Heber Lazo <heberlazo@gmail.com>
2023-06-08 17:12:58 -0500

    added active_nav_link

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -2,15 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="scroll"
 export default class extends Controller {
+  static targets = ['navlinks']
+  connect(){
+  }
   scrollToSection(event) {
     event.preventDefault();
-    const target = event.currentTarget.dataset.target;
-    const section = document.querySelector(`[data-scroll-target="${target}"]`);
+    const current_element = event.currentTarget
+    const section = document.querySelector(`[data-scroll-target="${current_element.dataset.target}"]`);
+    this.active_nav_link(this.navlinksTargets, current_element)
     if (section) {
       window.scrollTo({
         top: section.offsetTop,
         behavior: "smooth",
       });
     }
+  }
+
+  active_nav_link(links, current_element) {
+    links.forEach((link)=>{
+      link.classList.remove('active')
+    })
+    current_element.classList.add('active')
   }
 }
