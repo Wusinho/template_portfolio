cba83a320d7a673fe5cce217146d7a507cce9e8f
Heber Lazo <heberlazo@gmail.com>
2023-06-08 18:22:25 -0500

    Working scroll tracker

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -5,6 +5,9 @@ export default class extends Controller {
   static targets = ['navlinks']
   connect(){
   }
+  disconnect() {
+    // window.removeEventListener('scroll', this.handleScroll);
+  }
   scrollToSection(event) {
     event.preventDefault();
     const current_element = event.currentTarget
@@ -18,9 +21,41 @@ export default class extends Controller {
     }
   }
 
+  onScroll(e){
+    const scrollPosition = window.scrollY;
+    this.navlinksTargets.forEach((link) => {
+      const targetId = link.dataset.target;
+      const targetElement = document.getElementById(targetId);
+      const targetOffsetTop = targetElement.offsetTop;
+      const targetOffsetBottom = targetOffsetTop + targetElement.offsetHeight;
+      if (scrollPosition >= targetOffsetTop && scrollPosition < targetOffsetBottom) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
+      }
+    });
+  }
+
   active_nav_link(links, current_element) {
     links.forEach(link => link.classList.remove('active'));
     current_element.classList.add('active');
+  }
+
+
+  handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition)
+
+    // this.navlinkTargets.forEach((link) => {
+    //   const targetId = link.getAttribute('href').substring(1);
+    //   const targetElement = document.getElementById(targetId);
+    //
+    //   if (targetElement.offsetTop <= scrollPosition && targetElement.offsetTop + targetElement.offsetHeight > scrollPosition) {
+    //     link.classList.add('active');
+    //   } else {
+    //     link.classList.remove('active');
+    //   }
+    // });
   }
 
 }
