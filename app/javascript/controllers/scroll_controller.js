117ec1ffd7c8009c6277493d59b36d7b3df04d91
Heber Lazo <heberlazo@gmail.com>
2023-06-12 20:15:18 -0500

    corrected scroll navigation

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -21,35 +21,6 @@ export default class extends Controller {
     }
   }
 
-  // onScroll(e) {
-  //   const scrollPosition = window.scrollY;
-  //   const firstLink = this.navlinksTargets[0];
-  //   const firstLinkTargetId = firstLink.dataset.target;
-  //   const firstLinkTargetElement = document.getElementById(firstLinkTargetId);
-  //   const firstLinkTargetOffsetTop = firstLinkTargetElement.offsetTop;
-  //
-  //   // if (scrollPosition < firstLinkTargetOffsetTop) {
-  //   //   firstLink.classList.add('active');
-  //   // } else {
-  //   //   firstLink.classList.remove('active');
-  //   // }
-  //
-  //   this.navlinksTargets.forEach((link) => {
-  //     if (link !== firstLink) {
-  //       const targetId = link.dataset.target;
-  //       const targetElement = document.getElementById(targetId);
-  //       const targetOffsetTop = targetElement.offsetTop;
-  //       const targetOffsetBottom = targetOffsetTop + targetElement.offsetHeight;
-  //
-  //       if (scrollPosition >= targetOffsetTop && scrollPosition < targetOffsetBottom) {
-  //         link.classList.add('active');
-  //       } else {
-  //         link.classList.remove('active');
-  //       }
-  //     }
-  //   });
-  // }
-
   onScroll(e){
     const scrollPosition = window.scrollY;
     this.navlinksTargets.forEach((link) => {
@@ -57,12 +28,6 @@ export default class extends Controller {
       const targetElement = document.getElementById(targetId);
       const targetOffsetTop = targetElement.offsetTop - 40;
       const targetOffsetBottom = targetOffsetTop + targetElement.offsetHeight;
-      console.log('------------------')
-      console.log(scrollPosition)
-      console.log(targetOffsetTop)
-      console.log(targetOffsetBottom)
-
-      console.log('------------------')
       if (scrollPosition >= targetOffsetTop && scrollPosition < targetOffsetBottom) {
         link.classList.add('active');
       } else {
@@ -77,21 +42,5 @@ export default class extends Controller {
     current_element.classList.add('active');
   }
 
-
-  handleScroll = () => {
-    const scrollPosition = window.scrollY;
-    console.log(scrollPosition)
-
-    // this.navlinkTargets.forEach((link) => {
-    //   const targetId = link.getAttribute('href').substring(1);
-    //   const targetElement = document.getElementById(targetId);
-    //
-    //   if (targetElement.offsetTop <= scrollPosition && targetElement.offsetTop + targetElement.offsetHeight > scrollPosition) {
-    //     link.classList.add('active');
-    //   } else {
-    //     link.classList.remove('active');
-    //   }
-    // });
-  }
 
 }
