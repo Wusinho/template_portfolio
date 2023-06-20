7c4f0c5abd272b9ccdac1e5a3732ee8ab8902cb4
Heber Lazo <heberlazo@gmail.com>
2023-06-20 13:38:12 -0500

    back-up repo methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,14 @@ module ApplicationHelper
   end
 
   def social_icon(name)
-    social_name = "fa-brands fa-#{name.downcase} social_media"
+    other_icons = {
+      aws: 'devicon-amazonwebservices-original',
+    }
+    social_name = if other_icons.keys.include?(name.downcase.to_sym)
+                    other_icons[name.downcase.to_sym]
+                  else
+                    "fa-brands fa-#{name.downcase} social_media"
+                  end
     content_tag(:i, "", class: social_name)
   end
 end
