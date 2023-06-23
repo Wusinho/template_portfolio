bb02b94c933e1d6bf47a143ad6407a95a2af7fb4
Heber Lazo <heberlazo@gmail.com>
2023-06-23 11:46:02 -0500

    working hero animation

--- app/javascript/controllers/name_controller.js
+++ app/javascript/controllers/name_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ['name', 'replace', 'images'];
+  static targets = ['name', 'replace', 'images', 'box', 'box_2', 'bio', 'welcome'];
 
   connect() {
     this.imagesTarget.addEventListener('mouseenter', this.handleMouseEnter);
@@ -9,12 +9,21 @@ export default class extends Controller {
   }
 
   handleMouseEnter = (event) => {
+    console.log(this.welcomeTarget)
     this.replaceTarget.classList.add('replace')
+    this.boxTarget.classList.add('stretch')
+    this.box_2Target.classList.add('stretch_up')
     this.nameTarget.classList.add('vanish')
+    this.bioTarget.classList.add('vanish')
+    this.welcomeTarget.classList.add('vanish')
   }
 
   handleMouseLeave = (event) => {
     this.nameTarget.classList.remove('vanish')
+    this.boxTarget.classList.remove('stretch')
+    this.box_2Target.classList.remove('stretch_up')
+    this.bioTarget.classList.remove('vanish')
     this.replaceTarget.classList.remove('replace')
+    this.welcomeTarget.classList.remove('vanish')
   }
 }
