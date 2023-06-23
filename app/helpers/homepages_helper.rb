370d18676d37b4489cd56448b7e4fa2f77affe28
Heber Lazo <heberlazo@gmail.com>
2023-06-23 15:54:16 -0500

    Improved background

--- app/helpers/homepages_helper.rb
+++ app/helpers/homepages_helper.rb
@@ -5,6 +5,44 @@ module HomepagesHelper
     names << name
   end
 
+  def recruiters_list
+    [
+      'Programming Languages',
+      'Object-Oriented Programming',
+      'Agile Methodologies',
+      'Software Development Life Cycle (SDLC)',
+      'Web Development',
+      'Mobile Development',
+      'Database Management',
+      'API Integration',
+      'Version Control Systems',
+      'Test-Driven Development (TDD)',
+      'Debugging and Troubleshooting',
+      'Front-End Development',
+      'Back-End Development',
+      'Full-Stack Development',
+      'Software Architecture',
+      'DevOps and CI/CD',
+      'Cloud Computing Platforms',
+      'Big Data and Analytics',
+      'Machine Learning and AI',
+      'Security and Encryption',
+      'Quality Assurance and Testing',
+      'Performance Optimization',
+      'Scalability and Load Balancing',
+      'User Interface (UI) Design',
+      'User Experience (UX) Design',
+      'Software Documentation',
+      'Project Management',
+      'Problem-Solving Skills',
+      'Communication and Collaboration',
+      'Software Development Tools',
+      'Continuous Learning and Adaptability',
+      'AWS',
+      'Testing'
+    ]
+  end
+
   def cta
     'cta'
   end
